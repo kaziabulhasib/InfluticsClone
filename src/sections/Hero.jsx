f9b1ee9c84e0219510d0de46d0c54b27d0d6ceaa
kaziabulhasib <hasibkazi420@gmail.com>
2025-10-05 11:53:18 +0530

    contact me button created

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -19,12 +19,12 @@ const Hero = () => {
         <div className=''></div>
       </div>
       {/* RIGHT SIDE */}
-      <div className='flex gap-2 items-center'>
-        <button className='bg-blue-500 text-white px-4 py-2 rounded'>
-          Button 1
+      <div className='flex gap-2 items-center ml-8'>
+        <button className='text-white px-6 py-3 rounded-lg font-medium text-lg transition-all duration-300 ease-in-out bg-gradient-to-r from-[#fe9f54] to-[#f73197] hover:bg-gradient-to-r hover:from-[#f73197] hover:to-[#fe9f54]  shadow-md hover:shadow-lg  active:shadow-sm'>
+          About me
         </button>
-        <button className='bg-blue-500 text-white px-4 py-2 rounded'>
-          Button 2
+        <button className='bg-none hover:bg-[#f73197] border-gradient-to-r from-[#f28fcd] to-white border-1 hover:border-none  text-white px-6 py-3 rounded-lg  font-medium text-lg transition-all duration-300 ease-in-out  shadow-md hover:shadow-lg  active:shadow-sm'>
+          Contact me
         </button>
       </div>
     </div>
